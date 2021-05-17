d7ad86514662fe2761fe5b984a8b30ce03e8eb1c
MFTECH-code <51761169+MFTECH-code@users.noreply.github.com>
2021-05-17 00:09:45 -0300

    Exercicio 2 finalizado

--- rm86782/js/exercicios.js
+++ rm86782/js/exercicios.js
@@ -9,3 +9,30 @@ ex01.addEventListener("click", () => {
     alert("Veja o resultado final no console!")
     console.log(`Média: ${media}`);
 });
+
+const ex02 = document.querySelector(".ex02");
+ex02.addEventListener("click", () => {
+    let altura, peso;
+
+    altura = prompt("Digite sua altura(cm): ");
+    peso = prompt("Digite seu peso(Kg): ");
+
+    const imc = peso / (altura**2);
+
+    alert("Resultado no console!");
+
+    if (imc < 18.5) {
+        console.log("Abaixo do peso...");
+    } else if (imc >= 18.5 && imc < 24.9) {
+        console.log("Peso normal");
+    } else if (imc >= 24.9 && imc < 29.9) {
+        console.log("Sobrepeso...")
+    } else if (imc >= 29.9 && imc < 34.9) {
+        console.log("Obesidade grau I");
+    } else if (imc >= 34.9 && imc < 39.9) {
+        console.log("Obesidade grau II");
+    } else {
+        console.log("Obesidade grau III ou mórbida");
+    }
+    
+});
